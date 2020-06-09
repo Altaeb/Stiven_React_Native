12e6b8747e0ef5e2d800ab4e041dfe05ecd6d4cd
Altaeb <abd2010ly@gmail.com>
2020-06-09 08:34:58 +0200

    Building Lists

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+const ListScreen = () => {
+    return <Text>List Screen</Text>
+}
+
+const style = StyleSheet.create({});
+
+export default ListScreen;
